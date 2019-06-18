58b170655c978dc55bb51ff45048264db267e259
moldstud-hr <reg@moldstud.com>
2019-06-18 20:22:15 +0300

    test renders elements from person object added

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import { shallow, configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
-import City from '../components/city';
+import City from "../components/city";
 import { CircularProgress } from "@material-ui/core/index";
 
-const city = {isFetching: true, error: null, people: [] }
+const city = { isFetching: true, error: null, people: [] };
 
 it('renders without crashing', () => {
     shallow(<City city={city} />);
@@ -35,4 +35,23 @@ it('do not renders error', () => {
     const wrapper = shallow(<City city={{...city, error: false}} />);
     const error = <div>{errorText}</div>
     expect(wrapper.contains(error)).toEqual(false);
+});
+
+it("renders elements from person object", () => {
+  const people = [
+    {
+      id: 1,
+      name: "Tobus Quickwhistle",
+      thumbnail:
+        "http://www.publicdomainpictures.net/pictures/10000/nahled/thinking-monkey-11282237747K8xB.jpg",
+      age: 306,
+      hair_color: "Pink"
+    }
+  ];
+  const wrapper = mount(<City city={{ ...city, people }} />);
+  const { id, name, age, hair_color } = people[0];
+  expect(wrapper.contains(id)).toEqual(true);
+  expect(wrapper.contains(name)).toEqual(true);
+  expect(wrapper.contains(age)).toEqual(true);
+  expect(wrapper.contains(hair_color)).toEqual(true);
 });
